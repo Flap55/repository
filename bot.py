ede14835930f9a344e8191af55dde01459ffc04e
Flap55 <144260429+Flap55@users.noreply.github.com>
2023-09-07 17:58:21 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
+import os
+
 def run():
   print("Hello World!")
-  git add prueba.txt
-  git commit -m "commit message"
-  git push
+  os.system("git add prueba.txt")
+  os.system("git commit -m "commit message"")
+  os.system("git push")
